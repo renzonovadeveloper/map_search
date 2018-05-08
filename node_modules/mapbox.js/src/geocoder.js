bca1296dd2fa10507c74642013ad6a1133e33c8d
Fernando Tejada <rtejada@gmu.edu>
2018-05-08 11:13:01 -0400

    boiler plate from geojson.io

---
+++ node_modules/mapbox.js/src/geocoder.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var isArray = require('isarray'),
+    util = require('./util'),
+    urlhelper = require('./url'),
+    feedback = require('./feedback'),
+    request = require('./request');
+
+// Low-level geocoding interface - wraps specific API calls and their
+// return values.
+module.exports = function(url, options) {
+    if (!options) options = {};
+    var geocoder = {};
+
+    util.strict(url, 'string');
+
+    if (url.indexOf('/') === -1) {
+        url = urlhelper('/geocode/' + url + '/{query}.json', options.accessToken);
+    }
+
+    geocoder.getURL = function() {
+        return url;
+    };
+
+    geocoder.queryURL = function(_) {
+        var isObject = !(isArray(_) || typeof _ === 'string'),
+            query = isObject ? _.query : _;
+
+        if (isArray(query)) {
+            var parts = [];
+            for (var i = 0; i < query.length; i++) {
+                parts[i] = encodeURIComponent(query[i]);
+            }
+            query = parts.join(';');
+        } else {
+            query = encodeURIComponent(query);
+        }
+
+        feedback.record({ geocoding: query });
+
+        var url = L.Util.template(geocoder.getURL(), {query: query});
+
+        if (isObject && _.proximity) {
+            var proximity = L.latLng(_.proximity);
+            url += '&proximity=' + proximity.lng + ',' + proximity.lat;
+        }
+
+        return url;
+    };
+
+    geocoder.query = function(_, callback) {
+        util.strict(callback, 'function');
+
+        request(geocoder.queryURL(_), function(err, json) {
+            if (json && (json.length || json.features)) {
+                var res = {
+                    results: json
+                };
+                if (json.features && json.features.length) {
+                    res.latlng = [
+                        json.features[0].center[1],
+                        json.features[0].center[0]];
+
+                    if (json.features[0].bbox) {
+                        res.bounds = json.features[0].bbox;
+                        res.lbounds = util.lbounds(res.bounds);
+                    }
+                }
+                callback(null, res);
+            } else callback(err || true);
+        });
+
+        return geocoder;
+    };
+
+    // a reverse geocode:
+    //
+    //  geocoder.reverseQuery([80, 20])
+    geocoder.reverseQuery = function(_, callback) {
+        var q = '';
+
+        // sort through different ways people represent lat and lon pairs
+        function normalize(x) {
+            if (x.lat !== undefined && x.lng !== undefined) {
+                return x.lng + ',' + x.lat;
+            } else if (x.lat !== undefined && x.lon !== undefined) {
+                return x.lon + ',' + x.lat;
+            } else {
+                return x[0] + ',' + x[1];
+            }
+        }
+
+        if (_.length && _[0].length) {
+            for (var i = 0, pts = []; i < _.length; i++) {
+                pts.push(normalize(_[i]));
+            }
+            q = pts.join(';');
+        } else {
+            q = normalize(_);
+        }
+
+        request(geocoder.queryURL(q), function(err, json) {
+            callback(err, json);
+        });
+
+        return geocoder;
+    };
+
+    return geocoder;
+};
